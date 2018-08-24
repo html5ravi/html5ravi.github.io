760349a803afcac66e2ac96445483f112a98626e
Ravi G <ravi.g@spurtreetech.com>
2018-08-24 16:28:49 +0530

    updated wrapping code

--- worker-basic.min.js
+++ worker-basic.min.js
@@ -7,11 +7,12 @@
  */
 
 // tslint:disable:no-console
+(function() {
+    self.addEventListener('install', event => { self.skipWaiting(); });
 
-self.addEventListener('install', event => { self.skipWaiting(); });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-  self.registration.unregister().then(
-      () => { console.log('NGSW Safety Worker - unregistered old service worker'); });
-});
+    self.addEventListener('activate', event => {
+        event.waitUntil(self.clients.claim());
+        self.registration.unregister().then(
+            () => { console.log('NGSW Safety Worker - unregistered old service worker'); });
+    });
+})();
